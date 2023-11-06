b80abc32e3388666c9a2246240f326d67f1952da
heejunshin <abel9851@gmail.com>
2023-11-06 13:59:50 +0900

    study: redirect view in base view

--- django/views/generic/base.py
+++ django/views/generic/base.py
@@ -4,10 +4,12 @@ from functools import update_wrapper
 from django.core.exceptions import ImproperlyConfigured
 from django.template.response import TemplateResponse
 from django.utils.functional import classproperty
+from django.urls import reverse
 
 from django.utils.decorators import classonlymethod
 from django.http import (
-    HttpResponseNotAllowed, HttpResponse
+    HttpResponseNotAllowed, HttpResponse,
+    HttpResponseGone, HttpResponsePermanentRedirect, HttpResponseRedirect,
 )
 
 # django.request라는 로거 인스턴스를 가져오거나 생성
@@ -317,6 +319,71 @@ class View:
 # 231103 다음으로 할 것은 as_view의 update_wrapper 분석 하는것부터 시작.
 # view의 base.py 분석이 끝나면 HttpRepsonse 분석 할 것.
 # http_method_not_allowed, _allowed_methods, option 복습 완료
-# update_wrapper, __name__, __qualname__, __doc__, __annotations__, __dict__, update_wrapper의 updated, assigned 인자 습득완료
+# update_wrapper, __name__, __qualname__, __doc__, __annotations__, __dict__, 
+# update_wrapper의 updated, assigned 인자 습득완료
 # 231104에 위의 2줄 복습하기
 # update_wrapper 학습이 끝났으니 RedirectView 습득 시작하기
+# 231106 RedirectView 습득완료, 하지만 django.urls의 reverse나 httpresponse는 아직 습득하지 않음
+# templateview까지 습득 한뒤, reverse, httpresponse를 학습할 것
+# 231107에 RedirectView의 get_redirect_url 메소드, get 메소드 복습할 것
+
+
+# get 이외의 http 메소드로 접근할 시, 전부 get을 호출하는 것을 보면,
+# 이 RedirectView는 무조건 유저에게 redirect url을 제공하고
+# 그 url로 access 시키는 용도로 사용되는 것 같다.
+# 개발자로서는 url의 변경으로 인해 다른 url에 로직을 작성하고
+# 유저가 제대로 바뀐 곳으로 접속할 수 있도록 제어하는 용도로 사용하는 것 같다.
+class RedirectView(View):
+    """Provider a redirect on any GET request."""
+    permanent = False
+    url = None
+    pattern_name = None
+    query_string = False
+
+    def get_redirect_url(self, *args, **kwargs):
+        """
+        Return the URL Redirect to. Keyword arguments from the URL pattern
+        match generating the redirect request are provided as kwargs to this
+        method.
+        """
+        if self.url:
+            url = self.url % kwargs # example.com?pk=%%s
+        elif self.pattern_name: # user-detail
+            url = reverse(self.pattern_name, args=args, kwargs=kwargs)
+        else:
+            return None
+
+        if args and self.query_string:
+            url = "%s?%s" % (url, args)
+        return url
+
+    def get(self, request, *args, **kwargs):
+        url = self.get_redirect_url(*args, **kwargs)
+        # 응답을 어떤 status code로 할지 정한다.
+        # 검색엔진에 링크를 업데이트 시키냐 안시키냐.하는 부분까지도.
+        if url:
+            if self.permanent:
+                return HttpResponsePermanentRedirect(url)
+            else:
+                return HttpResponseRedirect(url)
+        else:
+            logger.warning(
+                'Gone: %s', request.path,
+                extra={'status_code': 410, 'request': request}
+            )
+            return HttpResponseGone()
+
+    def head(self, request, *args, **kwargs):
+        return self.get(request, *args, *kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def options(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
